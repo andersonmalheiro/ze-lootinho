8079eff9d246659f09a33a78132e4ecd6cb5981e
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-10-01 11:48:09 -0300

    improving kabum search

--- src/stores/kabum/search.py
+++ src/stores/kabum/search.py
@@ -1,27 +1,40 @@
+from typing import Sequence
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from src.entities.product import Product
 
 
-def search(product_name):
+def search(product_name) -> Sequence[Product]:
+    products: Sequence[Product] = []
+
     separator = '+'
     formated_name = separator.join(product_name.split(' '))
-    url = f'https://www.kabum.com.br/busca?query={formated_name}&page_number=1&page_size=20&facet_filters=&sort=price'
+
+    url = f'https://www.kabum.com.br/busca?query={formated_name}&page_number=1&page_size=40&facet_filters=&sort=price'
+
     html = urlopen(url)
     bs = BeautifulSoup(html, 'html.parser')
 
-    products = []
-
+    print("searching products...")
     product_cards = bs.find_all('div', {'class': 'productCard'})
 
     for card in product_cards:
         name = card.find('h2', {'class': 'nameCard'}).text
-        price = card.find('span', {'class': 'priceCard'}).text
+
+        price: str = card.find('span', {'class': 'priceCard'}).text.replace(".", "").replace(
+            ",", ".").replace("R$", "").strip()
+
+        formated_price = 0
+
+        try:
+            formated_price = float(price)
+        except:
+            pass
+
         unavailable = card.find('div', {'class': 'unavailableFooterCard'})
 
-        product = Product(name=name, price=price,
-                          available=not bool(unavailable), store="Kabum")
+        products.append(Product(name=name.lower(), price=formated_price,
+                                available=not bool(unavailable), store="Kabum"))
 
-        products.append(product)
-
+    print("products obtained.")
     return products
