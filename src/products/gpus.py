d90d84b8953082de99dc16ed7ed1b120573d5656
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-10-01 11:47:52 -0300

    adding product names to search

---
+++ src/products/gpus.py
@@ -0,0 +1,18 @@
+gpu_list = [
+    'gtx 1650',
+    'gtx 1650 super',
+    'gtx 1660',
+    'gtx 1660 super',
+    'rtx 2060',
+    'rtx 2060 super',
+    'rtx 2070',
+    'rtx 2070 super',
+    'rtx 2080',
+    'rtx 2080 super',
+    'rtx 3060',
+    'rtx 3060 super',
+    'rtx 3070',
+    'rtx 3070 super',
+    'rtx 3080',
+    'rtx 3080 super',
+]
